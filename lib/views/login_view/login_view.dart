82ac89678faaefb5118c8566735005cc45eefabb
ahmedkbenzid <ahmedkamel.benzid@ensi-uma.tn>
2026-01-04 12:17:26 +0100

    correct some issues

--- lib/views/login_view/login_view.dart
+++ lib/views/login_view/login_view.dart
@@ -1,7 +1,9 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/consts/fonts.dart';
 import 'package:flutter_application_1/consts/images.dart';
 import 'package:flutter_application_1/consts/strings.dart';
+import 'package:flutter_application_1/consts/colors.dart';
 import 'package:flutter_application_1/controllers/auth_controller.dart';
 import 'package:flutter_application_1/res/components/custom_button.dart';
 import 'package:flutter_application_1/res/components/custum_textfield.dart';
@@ -55,138 +57,106 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  resizeToAvoidBottomInset: true,
-  body: SafeArea(
-    child: Column(
-      children: [
-
-        // 🔹 Header
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              AppStyles.bold(
-                title: "MedDoc",
-                color: AppColors.blueColor,
-                size: AppSizes.size34,
-              ),
-              const Icon(Icons.menu),
-            ],
-          ),
-        ),
-
-        const SizedBox(height: 20),
-
-        // 🔹 Main content
-        Expanded(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-
-                // Logo + welcome
-                Image.asset(
-                  AppAssets.imgLogin,
-                  width: 200,
-                ),
-                10.heightBox,
-                AppStyles.bold(
-                  title: AppStrings.welcomeBack,
-                  size: AppSizes.size18,
-                ),
-                AppStyles.bold(title: AppStrings.weAreExcited),
-
-                30.heightBox,
-
-                // 🔹 Form
-                Form(
-                  child: Column(
-                    children: [
-                      CustumTextfield(hint: AppStrings.email),
-                      10.heightBox,
-                      CustumTextfield(hint: AppStrings.password),
-                      20.heightBox,
-
-                      // User type
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          AppStyles.bold(title: "I am a: "),
-                          Radio<String>(
-                            value: 'patient',
-                            groupValue: userType,
-                            onChanged: (value) {
-                              setState(() => userType = value!);
-                            },
-                            activeColor: AppColors.blueColor,
-                          ),
-                          AppStyles.normal(title: "Patient"),
-                          20.widthBox,
-                          Radio<String>(
-                            value: 'doctor',
-                            groupValue: userType,
-                            onChanged: (value) {
-                              setState(() => userType = value!);
-                            },
-                            activeColor: AppColors.blueColor,
-                          ),
-                          AppStyles.normal(title: "Doctor"),
-                        ],
-                      ),
-
-                      10.heightBox,
-
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: AppStyles.normal(
-                          title: AppStrings.forgetPassword,
-                        ),
-                      ),
-
-                      20.heightBox,
-
-                      CustomButton(
-                        buttonText: AppStrings.login,
-                        onTap: () {
-                          if (userType == 'doctor') {
-                            Get.to(() => const DocHome());
-                          } else {
-                            Get.to(() => const Home());
-                          }
-                        },
-                      ),
-
-                      20.heightBox,
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          AppStyles.normal(
-                            title: AppStrings.dontHaveAccount,
-                          ),
-                          8.widthBox,
-                          GestureDetector(
-                            onTap: () {
-                              Get.to(() => SignupView());
-                            },
-                            child: AppStyles.bold(
-                              title: AppStrings.signup,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+      body: Container(
+        margin: EdgeInsets.only(top: 40),
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    AppAssets.imgLogin,
+                    width: 200,
                   ),
-                ),
-              ],
+                  10.heightBox,
+                  AppStyles.bold(title: AppStrings.welcomeBack, size: AppSizes.size18),
+                  AppStyles.bold(title: AppStrings.weAreExcited),
+                ],
             ),
-          ),
-        ),
-      ],
-    ),
-  ),
-);
+            30.heightBox,
+            Expanded(
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    CustumTextfield(
+                      hint: AppStrings.email,
+                      textController: _emailController,
+                      validator: _authController.validateEmail,
+                    ),
+                    10.heightBox,
+                    CustumTextfield(
+                      hint: AppStrings.password,
+                      textController: _passwordController,
+                      validator: _authController.validatePassword,
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    ),
+                    20.heightBox,
+                    10.heightBox,
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: AppStyles.normal(title: AppStrings.forgetPassword),
+                    ),
+                    20.heightBox,
+                    Obx(() => _authController.isLoading.value
+                        ? const CircularProgressIndicator()
+                        : CustomButton(
+                            buttonText: AppStrings.login,
+                            onTap: _handleLogin,
+                          )),
+
+
+                    20.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AppStyles.normal(title: AppStrings.dontHaveAccount),
+                        8.widthBox,
+                        GestureDetector(
+                          onTap: () {
+                            Get.to(() => const SignupView());
+                          },
+                          child: AppStyles.bold(title: AppStrings.signup),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
